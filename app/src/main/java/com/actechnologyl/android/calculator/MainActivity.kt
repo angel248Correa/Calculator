6dea2b5b58544382c0df8b6a004946bb01950d88
angel <angelcorrea248@gmail.com>
2022-03-02 14:16:04 -0500

    Initial commit

---
+++ app/src/main/java/com/actechnologyl/android/calculator/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.actechnologyl.android.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCom
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
